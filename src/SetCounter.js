371db907a905985f08e99026890758d80829f98a
tom <lim1017@hotmail.com>
2023-03-23 23:58:20 -0400

    added setCounter, non controled

--- src/SetCounter.js
+++ src/SetCounter.js
@@ -1,13 +1,21 @@
+import { useDispatch } from 'react-redux';
+import { set } from './redux/actions';
+
 export const SetCounter = () => {
+  const dispatch = useDispatch();
+
   return (
     <section className="controls">
       <form
         onSubmit={(event) => {
           event.preventDefault();
+          const formData = new FormData(event.target);
+
+          dispatch(set(formData.get('number')));
         }}
       >
         <label htmlFor="set-to">Set Count</label>
-        <input id="set-to" type="number" />
+        <input name="number" id="set-to" type="number" />
         <input type="submit" />
       </form>
     </section>
